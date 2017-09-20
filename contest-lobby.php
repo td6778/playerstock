fa387abd13d466a38ea62847e0a10f7323f98916
Trent Davis <trendavisinc@gmail.com>
2017-09-20 18:45:44 -0500

    Added Entry Fee filter item, Fixed Scoring filter item

--- contest-lobby.php
+++ contest-lobby.php
@@ -27,23 +27,65 @@
 <div class="container">
 	<div class="filter row">
 		<div class="filter-1">
-			<div class="input-group">
-				<input type="text"  placeholder="Search Contests...">
-			</div>
+			<div>Contest Name</div>
+				<input type="text"  placeholder="Search...">
 		</div>
 		<div class="filter-2 filter-style-btn">
-			<div class="input-group-btn ">
-				<button type="button" class="btn btn-secondary dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Style
-				</button>
-				<div class="dropdown-menu filter-style-dropdown">
-					<div class="dropdown-item"><span class="all">ALL</span> <input type="checkbox" checked="checked"/></div>
-					<div class="dropdown-item"><span class="dk">DK</span> <input type="checkbox" /></div>
-					<div class="dropdown-item"><span class="fd">FD</span> <input type="checkbox" /></div>
-					<div class="dropdown-item"><span class="fdr">FDr</span> <input type="checkbox" /></div>
-				</div>
-			</div>
+			<div>Scoring</div>
+			<div id="entryFee-dropdown-from" class="ps-dropdown small" data-filter-type="min" data-selected-entry-value="0">
+					<a class="dropdown-filter" data-toggle="dropdown" aria-expanded="true">Style<span class="fa fa-caret-down"></span></a>
+					<ul class="dropdown-menu filter-menu" role="menu" aria-labelledby="entry-fee-dropdown-from">
+							<li><div class="style-filter-item first"><div class="all">ALL<input type="checkbox" checked="checked" /></div> </div></li>
+							<li><div class="style-filter-item"><div class="dk">DK<input type="checkbox" /> </div> </div></li>
+							<li><div class="style-filter-item"><div class="fd">FD<input type="checkbox" /></div> </div></li>
+							<li><div class="style-filter-item"><div class="fdr">FDr<input type="checkbox" /></div> </div></li>
+						</ul>
+					</div>
 		</div>
+
+		<div class="filter-3" data-filters-dropdown="entryFee">
+            <div>Entry Fee</div>
+            <div id="entryFee-dropdown-from" class="ps-dropdown small" data-filter-type="min" data-selected-entry-value="0">
+                <a class="dropdown-filter" data-toggle="dropdown" aria-expanded="true">Free<span class="fa fa-caret-down"></span></a>
+                <ul class="dropdown-menu filter-menu" role="menu" aria-labelledby="entry-fee-dropdown-from">
+                    <li><a href="#" data-entry-value="0" data-entry-type="entryMin">Free</a></li>
+                    <li><a href="#" data-entry-value="0.25" data-entry-type="entryMin">$.25</a></li>
+                    <li><a href="#" data-entry-value="1" data-entry-type="entryMin">$1</a></li>
+                    <li><a href="#" data-entry-value="2" data-entry-type="entryMin">$2</a></li>
+                    <li><a href="#" data-entry-value="3" data-entry-type="entryMin">$3</a></li>
+                    <li><a href="#" data-entry-value="5" data-entry-type="entryMin">$5</a></li>
+                    <li><a href="#" data-entry-value="10" data-entry-type="entryMin">$10</a></li>
+                    <li><a href="#" data-entry-value="30" data-entry-type="entryMin">$30</a></li>
+                    <li><a href="#" data-entry-value="50" data-entry-type="entryMin">$50</a></li>
+                    <li><a href="#" data-entry-value="100" data-entry-type="entryMin">$100</a></li>
+                    <li><a href="#" data-entry-value="200" data-entry-type="entryMin">$200</a></li>
+                    <li><a href="#" data-entry-value="500" data-entry-type="entryMin">$500</a></li>
+                    <li><a href="#" data-entry-value="1000" data-entry-type="entryMin">$1,000+</a></li>
+                </ul>
+            </div>
+            <span class="between-dropdowns">to</span>
+            <div id="entryFee-dropdown-to" class="ps-dropdown small" data-filter-type="max" data-selected-entry-value="null">
+                <a class="dropdown-filter" data-toggle="dropdown" aria-expanded="true">$1,000+<span class="fa fa-caret-down"></span></a>
+                <ul class="dropdown-menu filter-menu" role="menu" aria-labelledby="entry-fee-dropdown-to">
+                    <li><a href="#" data-entry-value="0" data-entry-type="entryMax">Free</a></li>
+                    <li><a href="#" data-entry-value="0.25" data-entry-type="entryMax">$.25</a></li>
+                    <li><a href="#" data-entry-value="1" data-entry-type="entryMax">$1</a></li>
+                    <li><a href="#" data-entry-value="2" data-entry-type="entryMax">$2</a></li>
+                    <li><a href="#" data-entry-value="3" data-entry-type="entryMax">$3</a></li>
+                    <li><a href="#" data-entry-value="5" data-entry-type="entryMax">$5</a></li>
+                    <li><a href="#" data-entry-value="10" data-entry-type="entryMax">$10</a></li>
+                    <li><a href="#" data-entry-value="30" data-entry-type="entryMax">$30</a></li>
+                    <li><a href="#" data-entry-value="50" data-entry-type="entryMax">$50</a></li>
+                    <li><a href="#" data-entry-value="100" data-entry-type="entryMax">$100</a></li>
+                    <li><a href="#" data-entry-value="200" data-entry-type="entryMax">$200</a></li>
+                    <li><a href="#" data-entry-value="500" data-entry-type="entryMax">$500</a></li>
+                    <li><a href="#" data-entry-value="1000" data-entry-type="entryMax">$1,000+</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+
 	</div>
 </div>
 <div class="container">
