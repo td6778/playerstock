aba3f1cdab45ddbb4ca801b6e5993ba166598a8f
Trent Davis <trentdavisinc@gmail.com>
2017-09-22 00:13:42 -0500

    Contest.php Header

--- contest.php
+++ contest.php
@@ -5,17 +5,19 @@
 
   <div class="row draft-row">
       <div class="col-12 draft-header">
-        <h1>NFL Legends Alpha Testing Tournament</h1>
+        <h1><i class="ion-ios-americanfootball">&nbsp;</i> NFL Legends Alpha Testing Tournament</h1>
       </div>
   </div>
   <div class="row draft-row">
-      <div class="col-5 draft-header">
+      <div class="col-5 draft-text draft-desc">
         <p>$0 IN CASH PRIZES ARE UP FOR GRABS. MULTI ENTRY FORMAT WITH A 5 ENTRY MAX PER PLAYER.</p>
+        <p class="draft-btn"><a class="btn btn-challenge">Challenge Friends</a></p>
       </div>
-      <div class="col-4 draft-header">
-        <p> GAME TIME: SEP 21 @ 8:25 PM ET LIVE IN: 00:26:18</p>
+      <div class="col-4 draft-text">
+        <h2>GAME TIME: OCT 11 @ 7:30 PM ET </h2>
+        <h3>LIVE IN: 00:26:18</h3>
       </div>
-      <div class="col-3 draft-header">
+      <div class="col-3 draft-text">
         <p>ENTRIES: 73 / 1000<br/>
           ENTRY FEE: FREE<br/>
           PRIZE PAYOUT: $0</p>
