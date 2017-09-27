eb00bf4228dff9ced98a26974344cedd48321e8e
Trent Davis <trentdavisinc@gmail.com>
2017-09-27 09:00:41 -0500

    Add mycontests.php

---
+++ mycontests.php
@@ -0,0 +1,14 @@
+<?php require('includes/header.php');?>
+<?php require('includes/navigation.php');?>
+<?php require('includes/error-reporting.php');?>
+
+<div class="container mycontest-container">
+
+
+</div>
+
+
+
+
+
+<?php require('includes/footer.php');?>
